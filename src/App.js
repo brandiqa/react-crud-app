55f47ae78204dddaed759eb01e22ecbd6dde7307
Michael Wanyoike <micwanyoike@gmail.com>
2020-01-22 17:16:07 +0300

    Installed Fomantic and setup routing

--- src/App.js
+++ src/App.js
@@ -6,32 +6,18 @@ import ContactFormPage from './pages/contact-form-page';
 class App extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar">
-          <div className="navbar-brand">
-            React Crud Demo
-          </div>
-          <div className="navbar-menu">
-            <div className="navbar-start">
-              <Link class="navbar-item" to="/">Home</Link>
-            </div>
-          </div>
-        </nav>
-        <div className="container">
-          <div className="ui two item menu">
-            <NavLink className="item" activeClassName="active" exact to="/">
-              Contacts List
-            </NavLink>
-            <NavLink className="item" activeClassName="active" exact to="/contacts/new">
-              Add Contact
-            </NavLink>
-          </div>
-          <Route exact path="/" component={ContactListPage}/>
-          <Route path="/contacts/new" component={ContactFormPage}/>
-          <Route path="/contacts/edit/:_id" component={ContactFormPage}/>
+      <div className="ui container">
+        <div className="ui two item menu">
+          <NavLink className="item" activeClassName="active" exact to="/">Contacts List</NavLink>
+          <NavLink className="item" activeClassName="active" exact to="/contacts/new">Add Contact</NavLink>
         </div>
+        <Route exact path="/" component={ContactListPage}/>
+        <Route path="/contacts/new" component={ContactFormPage}/>
+        <Route path="/contacts/edit/:_id" component={ContactFormPage}/>
       </div>
     );
   }
 }
+
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bulma';
+import 'fomantic-ui-css/semantic.min.css';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
