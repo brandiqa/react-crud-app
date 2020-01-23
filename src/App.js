d8e97c95383a43aa5b91b82fbeb323159b3e6957
Michael Wanyoike <micwanyoike@gmail.com>
2020-01-23 10:33:05 +0300

    Restored semantic ui react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { NavLink, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 import ContactListPage from './pages/contact-list-page';
 import ContactFormPage from './pages/contact-form-page';
 
 class App extends Component {
   render() {
     return (
-      <div className="ui container">
+      <Container>
         <div className="ui two item menu">
           <NavLink className="item" activeClassName="active" exact to="/">Contacts List</NavLink>
           <NavLink className="item" activeClassName="active" exact to="/contacts/new">Add Contact</NavLink>
@@ -14,7 +15,7 @@ class App extends Component {
         <Route exact path="/" component={ContactListPage}/>
         <Route path="/contacts/new" component={ContactFormPage}/>
         <Route path="/contacts/edit/:_id" component={ContactFormPage}/>
-      </div>
+      </Container>
     );
   }
 }
