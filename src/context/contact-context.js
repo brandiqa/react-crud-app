9748f4f4319a0cd01aefd6d5682650669fa8c6b5
Michael Wanyoike <micwanyoike@gmail.com>
2020-01-27 15:04:30 +0300

    Implemented useReducer

--- src/context/contact-context.js
+++ src/context/contact-context.js
@@ -1,15 +1,35 @@
 import React, {
-  useState,
+  useReducer,
   createContext
 } from 'react';
 
 export const ContactContext = createContext()
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_CONTACTS': {
+      return {
+        ...state,
+        contacts: action.payload
+      }
+    }
+    default:
+      throw new Error()
+  }
+}
+
+const initialState = {
+  contacts: [],
+  contact: null, // selected or new
+  loading: false,
+  errors: {}
+}
+
 export const ContactContextProvider = (props) => {
-  const [contacts, setContacts] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return(
-    <ContactContext.Provider value={[contacts, setContacts]}>
+    <ContactContext.Provider value={[state, dispatch]}>
       {props.children}
     </ContactContext.Provider>
   )
--- src/pages/contact-list-page.js
+++ src/pages/contact-list-page.js
@@ -1,19 +1,42 @@
 import React, { useContext, useEffect } from 'react';
 import ContactList from '../components/contact-list';
 import { ContactContext } from '../context/contact-context';
-import data from '../context/contact-data';
+
+const data = [
+  {
+    _id: "1",
+    name: {
+      first: "John",
+      last: "Doe"
+    },
+    phone: "555",
+    email: "john@gmail.com"
+  },
+  {
+    _id: "2",
+    name: {
+      first: "Bruce",
+      last: "Wayne"
+    },
+    phone: "777",
+    email: "bruce.wayne@gmail.com"
+  }
+];
 
 export default function ContactListPage() {
-  const [contacts, setContacts] = useContext(ContactContext);
+  const [state, dispatch] = useContext(ContactContext);
 
   useEffect(() => {
-    setContacts(data);
-  }, [setContacts])
+    dispatch({
+      type: 'FETCH_CONTACTS',
+      payload: data
+    });
+  }, [dispatch])
 
   return (
     <div>
       <h1>List of Contacts</h1>
-      <ContactList contacts={contacts} />
+      <ContactList contacts={state.contacts} />
     </div>
   )
 }
