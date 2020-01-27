4bcc6e492e228d898db8dbcb4fc5cbbf72a67182
Michael Wanyoike <micwanyoike@gmail.com>
2020-01-27 20:12:49 +0300

    Implemented async data fetching

--- src/pages/contact-list-page.js
+++ src/pages/contact-list-page.js
@@ -1,36 +1,20 @@
 import React, { useContext, useEffect } from 'react';
 import ContactList from '../components/contact-list';
 import { ContactContext } from '../context/contact-context';
-
-const data = [
-  {
-    _id: "1",
-    name: {
-      first: "John",
-      last: "Doe"
-    },
-    phone: "555",
-    email: "john@gmail.com"
-  },
-  {
-    _id: "2",
-    name: {
-      first: "Bruce",
-      last: "Wayne"
-    },
-    phone: "777",
-    email: "bruce.wayne@gmail.com"
-  }
-];
+import axios from 'axios';
 
 export default function ContactListPage() {
   const [state, dispatch] = useContext(ContactContext);
 
   useEffect(() => {
-    dispatch({
-      type: 'FETCH_CONTACTS',
-      payload: data
-    });
+    const fetchData = async () => {
+      const response = await axios.get('http://localhost:3030/contacts');
+      dispatch({
+        type: 'FETCH_CONTACTS',
+        payload: response.data.data || response.data // in case pagination is disabled
+      });
+    }
+    fetchData();
   }, [dispatch])
 
   return (
